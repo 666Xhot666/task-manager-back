c88173e9c765ece9115ffc8e392f8bb8c5e09a3c
Jack <36788406+666Xhot666@users.noreply.github.com>
2025-02-03 20:06:37 +0200

    update swagger config

--- src/core/configs/swagger.config.ts
+++ src/core/configs/swagger.config.ts
@@ -7,5 +7,13 @@ export function getSwaggerConfig() {
 			'Highly loaded backend for project and task management on NestJS.',
 		)
 		.setVersion('0.1')
+		.addBearerAuth(
+			{
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT',
+			},
+			'JWT',
+		)
 		.build();
 }
