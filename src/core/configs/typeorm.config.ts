6fefa7fcd58dd1828d249d2bb622d1ab4304909f
Jack <36788406+666Xhot666@users.noreply.github.com>
2025-02-03 10:29:13 +0200

    Add typeOrm + mysql

---
+++ src/core/configs/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+export function getTypeORMConfig(
+	configService: ConfigService,
+): TypeOrmModuleOptions {
+	return {
+		type: configService.getOrThrow<MysqlConnectionOptions['type']>(
+			'APPLICATION_DATABASE_PROVIDER',
+		),
+		host: configService.getOrThrow<string>('MYSQL_HOST'),
+		port: configService.getOrThrow<number>('MYSQL_PORT'),
+		username: configService.getOrThrow<string>('MYSQL_USER'),
+		password: configService.getOrThrow<string>('MYSQL_PASSWORD'),
+		database: configService.getOrThrow<string>('MYSQL_DATABASE'),
+	};
+}
